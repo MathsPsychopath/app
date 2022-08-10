1d69fefb9132dc2142efe02ad6bf5a7624d564d6
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-08-10 16:53:50 +0100

    added linkedin and google icon to buttons

--- authentication/LoginScreen.tsx
+++ authentication/LoginScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, Heading, VStack, FormControl, Input, Box } from 'native-base';
+import { Text, Heading, VStack, FormControl, Input, Box, HStack } from 'native-base';
 
 import Button from '../ui/Button';
 import Link from '../ui/Link';
+import { SvgUri } from 'react-native-svg';
 
 const LoginScreen = function () {
     return (
@@ -23,11 +24,41 @@ const LoginScreen = function () {
                     <Input />
                 </FormControl>
                 <Button>Submit</Button>
-                <Button variant="outlined">Login with Linkedin</Button>
+                <Button variant="outlined" position="relative">
+                    <PositionedButtonContent
+                        uri="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                        left="-100px"
+                    >
+                        Login with Google
+                    </PositionedButtonContent>
+                </Button>
+                <Button variant="outlined" position="relative">
+                    <PositionedButtonContent
+                        uri="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg"
+                        left="-95px"
+                    >
+                        Login with LinkedIn
+                    </PositionedButtonContent>
+                </Button>
                 <Link>Not yet a member? Sign up!</Link>
             </VStack>
         </Box>
     );
 };
+
+type PositionedProps = {
+    uri: string;
+    left: string;
+    children: React.ReactNode;
+};
+
+const PositionedButtonContent = ({ uri, left, children }: PositionedProps) => (
+    <HStack>
+        <Box position="absolute" left={left}>
+            <SvgUri uri={uri} width={25} height={25} />
+        </Box>
+        <Text>{children}</Text>
+    </HStack>
+);
 
 export default LoginScreen;
